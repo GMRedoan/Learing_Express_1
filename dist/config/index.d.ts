25ddc833a7ed3b35456b922d8f6440b67ac308f8
GMRedoan <redoangazi69@gmail.com>
2026-05-22 01:26:54 +0600

    finished

--- dist/config/index.d.ts
+++ dist/config/index.d.ts
@@ -1,6 +1,10 @@
 declare const config: {
     connection_string: string;
     port: string | undefined;
+    secret: string;
+    refresh_secret: string;
+    access_token_expire_time: string;
+    refresh_token_expire_time: string;
 };
 export default config;
 //# sourceMappingURL=index.d.ts.map
--- dist/config/index.js
+++ dist/config/index.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
+import { access } from "fs";
 import path from "path";
 dotenv.config({
     path: path.join(process.cwd(), '.env')
 });
 const config = {
     connection_string: process.env.CONNECTION_STRING,
-    port: process.env.PORT
+    port: process.env.PORT,
+    secret: process.env.JWT_SECRET_KEY,
+    refresh_secret: process.env.JWT_REFRESH_SECRET_KEY,
+    access_token_expire_time: process.env.ACCESS_TOKEN_EXPIRE_TIME,
+    refresh_token_expire_time: process.env.REFRESH_TOKEN_EXPIRE_TIME
 };
 export default config;
 //# sourceMappingURL=index.js.map
